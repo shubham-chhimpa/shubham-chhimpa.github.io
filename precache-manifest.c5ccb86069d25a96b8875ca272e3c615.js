2a4b616ea174a3a416c04e7e4766371a98dcdf24
shubhamchhimpa <chhimpa.shubh04@gmail.com>
2019-07-29 17:25:41 +0530

    v1

---
+++ precache-manifest.c5ccb86069d25a96b8875ca272e3c615.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c0221b21425141813a46da44864a3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "773fc0880f3012ca806d",
+    "url": "/static/css/main.a047ea91.chunk.css"
+  },
+  {
+    "revision": "33fba3f9293106f0b2b2",
+    "url": "/static/js/2.43d0257b.chunk.js"
+  },
+  {
+    "revision": "773fc0880f3012ca806d",
+    "url": "/static/js/main.0783abb9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
