b4e7529dc9f92fa40cdf61f678f9d7c36914ea18
shubhamchhimpa <chhimpa.shubh04@gmail.com>
2020-04-17 19:38:50 +0530

    added something

---
+++ precache-manifest.4bfcf12632db0b854d57846e4a8558c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f036b76410520091296732b6df5a13de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e5cef6daa1205b1f00f",
+    "url": "/static/css/2.d819a5bb.chunk.css"
+  },
+  {
+    "revision": "1e18d89bbac4ab49f9b3",
+    "url": "/static/css/main.5fc3e85f.chunk.css"
+  },
+  {
+    "revision": "6e5cef6daa1205b1f00f",
+    "url": "/static/js/2.0ae68e7e.chunk.js"
+  },
+  {
+    "revision": "1e18d89bbac4ab49f9b3",
+    "url": "/static/js/main.fc346617.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
